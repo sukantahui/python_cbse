b1f3c0c89995e8cb3c62f5d2f07ab9c70d9774ce
sukanta hui <huisukanta77@gmail.com>
2020-09-03 12:08:28 +0530

    word length added

---
+++ longest_word.py
@@ -0,0 +1,19 @@
+def longest_word(filename):
+    with open(filename, 'r') as infile:
+              words = infile.read().split()
+    max_len = len(max(words, key=len))
+    return [word for word in words if len(word) == max_len]
+
+# print(longest_word('myText.txt'))
+
+with open('myText.txt', 'r') as f:
+    words = f.read().split()
+    print(words)
+    # for line in f:
+    #     print(line)
+        # if len(line) > large_line_len:
+        #     large_line_len = len(line)
+        #     large_line = line
+
+
+
